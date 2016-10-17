f8cb9e2ab5c2b8217aae2f40ba19d31c6cce746b
nbappi <bappi.cse562@gmail.com>
2016-10-17 11:59:17 +0600

    Late Static Binding in PHP

--- late_static_binding.php
+++ late_static_binding.php
@@ -1,7 +1,29 @@
 <?php
 
+// use self parent class define 
+// use static parent and child class eccess.
 class php{
 	public static function framework(){
-		echo "Laravel ia a framework";
+		echo static::getClass();
+	}
+
+	public static function getClass(){
+       return __class__;
 	}
 }
+
+class phpChild extends php{
+	public static function getClass(){
+		return __class__;
+	}
+}
+echo "Use static :<br />";
+$php =  new php;
+$php->framework();
+
+echo "<hr />";
+
+$phpChild = new phpChild;
+$phpChild->framework();
+
+
