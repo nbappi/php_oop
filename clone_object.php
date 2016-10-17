7f5d5b7b3c16a0f8b5f2c371612d34a3f350f7e9
nbappi <bappi.cse562@gmail.com>
2016-10-17 12:30:08 +0600

    Object clone in php

---
+++ clone_object.php
@@ -0,0 +1,34 @@
+<?php
+class php{
+	private $cat, $frame;
+
+	public function setCat($a){
+		$this->cat = $a;
+	}
+
+	public function getCat(){
+		return $this->cat;
+	}
+
+	public function setFrame($a){
+		$this->frame = $a;
+	}
+
+	public function getFrame(){
+		return $this->frame;
+	}
+}
+
+$php = new php;
+$php->setCat("OOP");
+$php->setFrame("Spring");
+
+// $java = $php;
+$java = clone $php;
+$java->setFrame("Codeigniter");
+
+echo $php->getCat()."<br />";
+echo $php->getFrame()."<br />";
+
+echo $java->getCat()."<br />";
+echo $java->getFrame()."<br />";
